fad42300da9fa9b511ed61970b9229f110272957
LinLin <mylinh.khanhhung@gmail.com>
2023-11-24 21:05:59 +0700

    thêm utils auth(Linh)

---
+++ src/main/java/com/app/utils/Auth.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.app.utils;
+
+
+import com.app.Daos.NhanVienDao;
+import com.app.Entitys.NhanVien;
+import com.app.Entitys.TaiKhoan;
+
+
+/**
+ *
+ * @author MY LINH
+ */
+public class Auth {
+    static NhanVienDao dao = new NhanVienDao();
+    public static TaiKhoan user = null;
+    public static void clear(){
+        Auth.user =  null;
+    }
+    public static boolean isLogin(){
+        return Auth.user != null;
+    }
+    public static boolean isManager(){
+        NhanVien nv = new NhanVien();
+        nv = dao.selectById(user.getMaNhanVien());
+        return Auth.isLogin() && nv.isMaVaitro() ;
+    }
+//      public static void checkLogin(Component btnXoa){
+//        if(!Auth.isLogin()){
+//            System.exit(0);
+//        }
+//        else if(btnXoa == null){
+//            
+//        } else if(!isManager()){
+//            btnXoa.setEnabled(false);
+//        }
+//    }
+}
